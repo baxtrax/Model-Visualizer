6e6ec3aea32452b36523f688bf03c92591affa5e
brad <34373485+baxtrax@users.noreply.github.com>
2023-06-02 16:25:45 -0400

    Resurvie calls working

--- scripts/canvas.js
+++ scripts/canvas.js
@@ -2,12 +2,14 @@ const maxDistance = 100; // Adjust this value to control attraction range
 const maxMovement = 25; // Adjust this value to control maximum movement
 
 var buttons = null;
+var buttonContainer = null;
 var canvas = null;
 var ctx = null;
 
 // Wait for DOM to load to then fetch elements
 document.addEventListener('DOMContentLoaded', function() {
     buttons = document.querySelectorAll('.attracted-button');
+    buttonContainer = document.getElementById('button-container')
     canvas = document.getElementById('lineCanvas');
     ctx = canvas.getContext('2d');
 
@@ -17,6 +19,12 @@ document.addEventListener('DOMContentLoaded', function() {
     // Initial setup
     handleResize();
     requestAnimationFrame(repeatEveryAnimationFrame);
+
+    console.log("Creating new buttons")
+    rootNode.getChildren().forEach(addButton)
+
+    console.log("Updating button list")
+    updateButtonList()
 });
 
 // Update button attraction
@@ -47,23 +55,26 @@ document.addEventListener('mousemove', function(event) {
 function updateLines() {
   ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
 
-  const rect1 = buttons[0].getBoundingClientRect();
-  const rect2 = buttons[1].getBoundingClientRect();
-  const rect3 = buttons[2].getBoundingClientRect();
-
-  const center1X = rect1.left + rect1.width / 2;
-  const center1Y = rect1.top + rect1.height / 2;
-
-  const center2X = rect2.left + rect2.width / 2;
-  const center2Y = rect2.top + rect2.height / 2;
-
-  const center3X = rect3.left + rect3.width / 2;
-  const center3Y = rect3.top + rect3.height / 2;
-
   ctx.beginPath();
-  ctx.moveTo(center1X, center1Y);
-  ctx.lineTo(center2X, center2Y);
-  ctx.lineTo(center3X, center3Y);
+  for (let i = 0; i < buttons.length-1; i++) {
+    const rect1 = buttons[i].getBoundingClientRect();
+    const rect2 = buttons[i+1].getBoundingClientRect();
+
+    const center1X = rect1.left + rect1.width / 2;
+    const center1Y = rect1.top + rect1.height / 2;
+  
+    const center2X = rect2.left + rect2.width / 2;
+    const center2Y = rect2.top + rect2.height / 2;
+
+    if (i==0) {
+      ctx.moveTo(center1X, center1Y);
+      ctx.lineTo(center2X, center2Y);
+    } else {
+      ctx.lineTo(center1X, center1Y);
+      ctx.lineTo(center2X, center2Y);
+    }
+
+  }
   ctx.stroke();
 }
 
@@ -81,4 +92,73 @@ function handleResize() {
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
     // updateLines();
+}
+
+const rootNode = new TreeNode('model');
+const rootChildNode1 = new TreeNode('conv1');
+const rootChildNode2 = new TreeNode('bn1');
+const rootChildNode3 = new TreeNode('maxpool');
+const rootChildNode4 = new TreeNode('classifier')
+const rootChildNode5 = new TreeNode('layer1')
+const rootChildNode6 = new TreeNode('layer2')
+const rootChildNode7 = new TreeNode('layer3')
+const rootChildNode8 = new TreeNode('layer4')
+
+const layer1ChildNode1 = new TreeNode('0')
+const layer10ChildNode1 = new TreeNode('conv1')
+const layer10ChildNode2 = new TreeNode('bn1')
+const layer10ChildNode3 = new TreeNode('relu')
+const layer10ChildNode4 = new TreeNode('conv2')
+const layer10ChildNode5 = new TreeNode('bn2')
+
+const layer1ChildNode2 = new TreeNode('1')
+const layer11ChildNode1 = new TreeNode('conv1')
+const layer11ChildNode2 = new TreeNode('bn1')
+const layer11ChildNode3 = new TreeNode('relu')
+const layer11ChildNode4 = new TreeNode('conv2')
+const layer11ChildNode5 = new TreeNode('bn2')
+
+rootNode.addChild(rootChildNode1)
+rootNode.addChild(rootChildNode2)
+rootNode.addChild(rootChildNode3)
+rootNode.addChild(rootChildNode4)
+rootNode.addChild(rootChildNode5)
+rootNode.addChild(rootChildNode6)
+rootNode.addChild(rootChildNode7)
+rootNode.addChild(rootChildNode8)
+
+rootChildNode5.addChild(layer1ChildNode1)
+layer1ChildNode1.addChild(layer10ChildNode1)
+layer1ChildNode1.addChild(layer10ChildNode2)
+layer1ChildNode1.addChild(layer10ChildNode3)
+layer1ChildNode1.addChild(layer10ChildNode4)
+layer1ChildNode1.addChild(layer10ChildNode5)
+
+
+rootChildNode5.addChild(layer1ChildNode2)
+layer1ChildNode2.addChild(layer11ChildNode1)
+layer1ChildNode2.addChild(layer11ChildNode2)
+layer1ChildNode2.addChild(layer11ChildNode3)
+layer1ChildNode2.addChild(layer11ChildNode4)
+layer1ChildNode2.addChild(layer11ChildNode5)
+
+function addButton(item) {
+  console.log("Addig new button...")
+  const newButton = document.createElement('button');
+  newButton.textContent = item.getValue()
+
+  buttonChildren = item.getChildren()
+  if (buttonChildren.length != 0) {
+    newButton.className = "attracted-button clickable"
+    newButton.addEventListener('click', () => {
+      buttonContainer.innerHTML = "" // Clear current buttons
+      item.getChildren().forEach(addButton)
+    });
+    
+
+  } else {
+    newButton.className = "attracted-button"
+  }
+
+  buttonContainer.appendChild(newButton);
 }
--- scripts/tree.js
+++ scripts/tree.js
@@ -1,171 +1,37 @@
-class TreeNode {
-  constructor(value) {
-    this.value = value;
-    this.children = [];
-  }
-
-  addChild(node) {
-    if (!(node instanceof TreeNode)) {
-      throw new Error('Invalid node. Node must be an instance of TreeNode.');
+  class TreeNode {
+    constructor(value) {
+      this.value = value;
+      this.children = [];
     }
 
-    this.children.push(node);
-  }
+    addChild(node) {
+      if (!(node instanceof TreeNode)) {
+        throw new Error('Invalid node. Node must be an instance of TreeNode.');
+      }
 
-  removeChild(node) {
-    const index = this.children.indexOf(node);
-    if (index !== -1) {
-      this.children.splice(index, 1);
-    }
-  }
-
-  getChild(index) {
-    if (index < 0 || index >= this.children.length) {
-      throw new Error('Index out of bounds.');
+      this.children.push(node);
     }
 
-    return this.children[index];
+    removeChild(node) {
+      const index = this.children.indexOf(node);
+      if (index !== -1) {
+        this.children.splice(index, 1);
+      }
+    }
+
+    getChild(index) {
+      if (index < 0 || index >= this.children.length) {
+        throw new Error('Index out of bounds.');
+      }
+
+      return this.children[index];
+    }
+
+    getChildren() {
+      return this.children;
+    }
+
+    getValue() {
+      return this.value;
+    }
   }
-
-  getChildren() {
-    return this.children;
-  }
-
-  getValue() {
-    return this.value;
-  }
-}
-
-// ResNet(
-//   (conv1): Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-//   (bn1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//   (relu): ReLU(inplace=True)
-//   (maxpool): MaxPool2d(kernel_size=3, stride=2, padding=1, dilation=1, ceil_mode=False)
-//   (layer1): Sequential(
-//     (0): BasicBlock(
-//       (conv1): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//     )
-//     (1): BasicBlock(
-//       (conv1): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//     )
-//   )
-//   (layer2): Sequential(
-//     (0): BasicBlock(
-//       (conv1): Conv2d(64, 128, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (downsample): Sequential(
-//         (0): Conv2d(64, 128, kernel_size=(1, 1), stride=(2, 2), bias=False)
-//         (1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       )
-//     )
-//     (1): BasicBlock(
-//       (conv1): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(128, 128, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//     )
-//   )
-//   (layer3): Sequential(
-//     (0): BasicBlock(
-//       (conv1): Conv2d(128, 256, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (downsample): Sequential(
-//         (0): Conv2d(128, 256, kernel_size=(1, 1), stride=(2, 2), bias=False)
-//         (1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       )
-//     )
-//     (1): BasicBlock(
-//       (conv1): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(256, 256, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//     )
-//   )
-//   (layer4): Sequential(
-//     (0): BasicBlock(
-//       (conv1): Conv2d(256, 512, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (downsample): Sequential(
-//         (0): Conv2d(256, 512, kernel_size=(1, 1), stride=(2, 2), bias=False)
-//         (1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       )
-//     )
-//     (1): BasicBlock(
-//       (conv1): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn1): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//       (relu): ReLU(inplace=True)
-//       (conv2): Conv2d(512, 512, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1), bias=False)
-//       (bn2): BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
-//     )
-//   )
-//   (avgpool): AdaptiveAvgPool2d(output_size=(1, 1))
-//   (fc): Linear(in_features=512, out_features=10, bias=True)
-// )
-
-// Example usage:
-const rootNode = new TreeNode('model');
-const rootChildNode1 = new TreeNode('conv1');
-const rootChildNode2 = new TreeNode('bn1');
-const rootChildNode3 = new TreeNode('maxpool');
-const rootChildNode4 = new TreeNode('classifier')
-const rootChildNode5 = new TreeNode('layer1')
-const rootChildNode6 = new TreeNode('layer2')
-const rootChildNode7 = new TreeNode('layer3')
-const rootChildNode8 = new TreeNode('layer4')
-
-const layer1ChildNode1 = new TreeNode('0')
-const layer10ChildNode1 = new TreeNode('conv1')
-const layer10ChildNode2 = new TreeNode('bn1')
-const layer10ChildNode3 = new TreeNode('relu')
-const layer10ChildNode4 = new TreeNode('conv2')
-const layer10ChildNode5 = new TreeNode('bn2')
-
-const layer1ChildNode2 = new TreeNode('1')
-const layer11ChildNode1 = new TreeNode('conv1')
-const layer11ChildNode2 = new TreeNode('bn1')
-const layer11ChildNode3 = new TreeNode('relu')
-const layer11ChildNode4 = new TreeNode('conv2')
-const layer11ChildNode5 = new TreeNode('bn2')
-
-rootNode.addChild(rootChildNode1)
-rootNode.addChild(rootChildNode2)
-rootNode.addChild(rootChildNode3)
-rootNode.addChild(rootChildNode4)
-rootNode.addChild(rootChildNode5)
-rootNode.addChild(rootChildNode6)
-rootNode.addChild(rootChildNode7)
-rootNode.addChild(rootChildNode8)
-
-rootChildNode5.addChild(layer1ChildNode1)
-layer1ChildNode1.addChild(layer10ChildNode1)
-layer1ChildNode1.addChild(layer10ChildNode2)
-layer1ChildNode1.addChild(layer10ChildNode3)
-layer1ChildNode1.addChild(layer10ChildNode4)
-layer1ChildNode1.addChild(layer10ChildNode5)
-
-
-rootChildNode5.addChild(layer1ChildNode2)
-layer1ChildNode2.addChild(layer11ChildNode1)
-layer1ChildNode2.addChild(layer11ChildNode2)
-layer1ChildNode2.addChild(layer11ChildNode3)
-layer1ChildNode2.addChild(layer11ChildNode4)
-layer1ChildNode2.addChild(layer11ChildNode5)
